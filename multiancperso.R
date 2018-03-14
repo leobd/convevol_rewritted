afa2ce80e5bfcac2c2a94ef6777d4b1d2c8468cf
leobd <botton.divet.leo@gmail.com>
2018-03-14 15:50:44 +0100

    Create multiancperso.R

---
+++ multiancperso.R
@@ -0,0 +1,25 @@
+##########################################################################################
+##########################################################################################
+###			            Stayton's multianc (convevol R package; 2014) parallelized		      ##
+###		                      	from Botton-Divet et al. 2016 				                   	##
+###			                   please contact lbottondivet@mnhn.fr if needed		           	##
+###			                      or visit https://github.com/leobd 				                ##
+##########################################################################################
+##########################################################################################
+
+multiancperso<-function (phyl, phendata) 
+{
+    if (class(phyl) != "phylo") 
+        stop("your tree must be class 'phylo.'")
+    if (nrow(phendata) != length(phyl$tip)) 
+        stop("your data matrix must have the same number of rows as tips in the tree.")
+    if (is.null(rownames(phendata))) {
+        warning("no row names for data.  Assuming that the rows are in the same order as tips.")
+        rownames(X) <- phyl$tip.label
+    }
+    firstvar <- fastAnc(phyl, phendata[, 1])
+    
+  allancstates<-matrix(data=unlist(foreach(i=1:ncol(phendata))%dopar%{fastAnc(phyl,phendata[,i])}),nrow=length(firstvar),ncol=ncol(phendata),byrow=F)
+    colnames(allancstates) <- colnames(phendata)
+    alldata <- rbind(phendata, allancstates)
+}
